8c8ff3432c0f4bc1646623eec6ea3ef7d950eaf8
Nguyen Pham <78258269+CornyCodingCorn@users.noreply.github.com>
2021-03-19 22:15:16 +0700

    slep

--- Banking/View/MainWin.xaml.cs
+++ Banking/View/MainWin.xaml.cs
@@ -22,6 +22,7 @@ namespace Banking.View
         public MainWin(CornUI.Controls.CwndInfo info) : base(info)
         {
             InitializeComponent();
+            Grid content = GetTemplateChild("Content") as Grid;
         }
     }
 }
